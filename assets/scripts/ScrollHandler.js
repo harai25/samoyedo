add262b0eadf218a6c9106dab7ffd32d4ae2e90a
Evgeniy Araslanov <araslanov21@gmail.com>
2022-11-16 21:08:36 +0400

    sticky and anim to top

--- assets/scripts/ScrollHandler.js
+++ assets/scripts/ScrollHandler.js
@@ -1,12 +1,12 @@
-// const cloud1El = document.getElementById('cloud-1')
-// const cloud2El = document.getElementById('cloud-2')
+const cloud1El = document.getElementById('cloud-1')
+const cloud2El = document.getElementById('cloud-2')
 const mainTitleEl = document.getElementById('main-title')
 const mainTitle2El = document.getElementById('main-title2')
 
 function mainHandle() {
-    const transformTitle = `translateY(${window.scrollY/3.5}px) scale(${1+window.scrollY/mainPosition.height/6})`
-    // cloud1El.style.transform = `translateY(${-window.scrollY/9}px) scale(${1+window.scrollY/mainPosition.height/9})`
-    // cloud2El.style.transform = `translateY(${-window.scrollY/12}px) scale(${1+window.scrollY/mainPosition.height/12})`
+    const transformTitle = `translateY(${-window.scrollY/3.5}px) scale(${1+window.scrollY/mainPosition.height/6})`
+    cloud1El.style.transform = `translateY(${-window.scrollY/2}px) scale(${1+window.scrollY/mainPosition.height/9})`
+    cloud2El.style.transform = `translateY(${-window.scrollY/3}px) scale(${1+window.scrollY/mainPosition.height/12})`
     mainTitleEl.style.transform = transformTitle
     mainTitle2El.style.transform = transformTitle
 }
