4c9b08114dceb78e21f4c63ab19c5e43447d0e80
Evgeniy Araslanov <araslanov21@gmail.com>
2022-11-16 21:43:54 +0400

    header

--- assets/scripts/ScrollHandler.js
+++ assets/scripts/ScrollHandler.js
@@ -2,6 +2,7 @@ const cloud1El = document.getElementById('cloud-1')
 const cloud2El = document.getElementById('cloud-2')
 const mainTitleEl = document.getElementById('main-title')
 const mainTitle2El = document.getElementById('main-title2')
+const headerEl = document.getElementById('header')
 
 function mainHandle() {
     const transformTitle = `translateY(${-window.scrollY/3.5}px) scale(${1+window.scrollY/mainPosition.height/6})`
@@ -13,6 +14,12 @@ function mainHandle() {
 
 function scrollHandler(e) {
     const scrollY = window.scrollY
+
+    if (scrollY > window.innerHeight) {
+        headerEl.classList.add('sticky-header')
+    } else {
+        headerEl.classList.remove('sticky-header')
+    }
 
     if (scrollY < mainPosition.bottom - mainPosition.height/4) {
         mainHandle()
