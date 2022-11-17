bc28712e928714929ef8bb60d1eb65549b8ef3bb
Harai25 <gundorov2001@gmail.com>
2022-11-18 00:41:14 +0400

    circle wip

--- assets/scripts/ThemeChange.js
+++ assets/scripts/ThemeChange.js
@@ -2,17 +2,59 @@ let isDark = true
 const mainEl = document.getElementById('main')
 const overflowLightEl = document.getElementById('overflow-light')
 const overflowDarkEl = document.getElementById('overflow-dark')
+
+function setMainStyle(el, offset) {
+    const calc = offset / 2
+    el.style.width = window.innerWidth + window.innerHeight + 'px'
+    el.style.height = window.innerWidth + window.innerHeight + 'px'
+    el.style.top = -calc + 'px'
+    el.style.left = -calc + 'px'
+
+    const mainTheme = el.children[0]
+    mainTheme.style.top = calc + 'px'
+    mainTheme.style.left = calc + 'px'
+}
+
+function setHiddenStyle(el) {
+    el.style.width = '10px'
+    el.style.height = '10px'
+    el.style.top = 'calc(50vh + 50px)'
+    el.style.left = '50vw'
+
+    const mainTheme = el.children[0]
+    mainTheme.style.top = null
+    mainTheme.style.left = null
+}
+
+function swapStyles(el1, el2) {
+    el1.classList.remove('overflow-hide')
+    el1.classList.add('overflow-full')
+    el2.classList.remove('overflow-full')
+    el2.classList.add('overflow-hide')
+}
+
+isDeleteDefault = false
+
 mainEl.addEventListener('click', () => {
-    if (isDark) {
-        overflowDarkEl.classList.remove('overflow-full')
-        overflowDarkEl.classList.add('overflow-hide')
-        overflowLightEl.classList.remove('overflow-hide')
-        overflowLightEl.classList.add('overflow-full')
-    } else {
-        overflowLightEl.classList.remove('overflow-full')
-        overflowLightEl.classList.add('overflow-hide')
-        overflowDarkEl.classList.remove('overflow-hide')
-        overflowDarkEl.classList.add('overflow-full')
+    if (!isDeleteDefault) {
+        overflowDarkEl.classList.remove('overflow-default')
+        isDeleteDefault = true
     }
+
     isDark = !isDark
+    let offset = 0
+    if (window.innerWidth > window.innerHeight) {
+        offset = window.innerHeight
+    } else {
+        offset = window.innerWidth 
+    }
+    if (isDark) {
+        setHiddenStyle(overflowLightEl)
+        setMainStyle(overflowDarkEl, offset)
+        swapStyles(overflowDarkEl, overflowLightEl)
+    } else {
+        setHiddenStyle(overflowDarkEl)
+        setMainStyle(overflowLightEl, offset)
+        swapStyles(overflowLightEl, overflowDarkEl)
+    }
 })
