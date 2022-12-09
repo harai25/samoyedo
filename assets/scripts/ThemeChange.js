316c11e7459990a4d72abeb4553328f0c79e78c4
gundorovns <gundorovns@63pokupki.ru>
2022-12-09 23:16:38 +0400

    together complete

--- assets/scripts/ThemeChange.js
+++ assets/scripts/ThemeChange.js
@@ -1,18 +1,9 @@
-let isDark = true
-const mainEl = document.getElementById('main')
-const overflowLightEl = document.getElementById('overflow-light')
-const overflowDarkEl = document.getElementById('overflow-dark')
-mainEl.addEventListener('click', () => {
-    if (isDark) {
-        overflowDarkEl.classList.remove('overflow-full')
-        overflowDarkEl.classList.add('overflow-hide')
-        overflowLightEl.classList.remove('overflow-hide')
-        overflowLightEl.classList.add('overflow-full')
-    } else {
-        overflowLightEl.classList.remove('overflow-full')
-        overflowLightEl.classList.add('overflow-hide')
-        overflowDarkEl.classList.remove('overflow-hide')
-        overflowDarkEl.classList.add('overflow-full')
-    }
-    isDark = !isDark
+const themes = document.querySelectorAll('.main-theme')
+
+document.addEventListener('click', () => {
+    themes.forEach((e) => {
+        e.classList.toggle('visible-theme')
+
+        e.classList.toggle('hidden-theme')
+    })
 })
